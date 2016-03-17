7c79bf4a839810a84ef5102e4d062664d4c31649
juragan360 <ariefda@hotmail.com>
2016-03-17 14:21:47 +0700

    apply rpc dial

--- client.go
+++ client.go
@@ -3,6 +3,7 @@ package sebarmod
 import (
 	"github.com/eaciit/toolkit"
     "errors"
+    "net/rpc"
 )
 
 type ClientStatus int
@@ -22,6 +23,7 @@ func NewClient(host string, config toolkit.M) *Client{
 type Client struct {
     Host string
     
+    rpcclient *rpc.Client
     config toolkit.M
 	state ClientStatus
 }
@@ -45,13 +47,30 @@ func (c *Client) Connect() error {
     if c.Host=="" {
         return errors.New("client.Connect: Host is empty")
     }
+    
+    client, econnect := rpc.Dial("tcp", c.Host)
+    if econnect!=nil {
+        return errors.New("client.Connect: " + econnect.Error())
+    }
+    c.rpcclient = client
 	return nil
 }
 
 /*Call call fn on server*/
-func (c *Client) Call(name string, data toolkit.M) *toolkit.Result {
-	ret := toolkit.NewResult()
-    return ret.SetErrorTxt("mod.Client.Call: no data is being returned")
+func (c *Client) Call(methodName string, in toolkit.M) *toolkit.Result {
+	if c.rpcclient == nil {
+		return toolkit.NewResult().SetErrorTxt(toolkit.Sprintf("Unable to call %s.%s, no connection handshake", c.Host, methodName))
+	}
+	if in == nil {
+		in = toolkit.M{}
+	}
+	out := toolkit.NewResult()
+	in["method"] = methodName
+	e := c.rpcclient.Call("Rpc.Do", in, out)
+	if e != nil {
+		return out.SetErrorTxt(c.Host + "." + methodName + " Fail: " + e.Error())
+	}
+	return out
 }
 
 /*IsConnected Check if client is connected */
